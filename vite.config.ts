860cbfb4370340563b82fee06676c4161ef888c7
MH4GF <h.miyagi.cnw@gmail.com>
2023-04-07 23:26:33 +0900

    feat: setup crxjs for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "WYSIWYG Editor for GitHub",
+  version: "1.0.0",
+  action: {
+    default_popup: "index.html",
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), crx({ manifest })],
+});
