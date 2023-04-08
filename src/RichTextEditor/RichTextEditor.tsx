641d33da1bfbbc2803bf8f1261f50c8d0ee0b098
MH4GF <h.miyagi.cnw@gmail.com>
2023-04-08 19:11:55 +0900

    feat: get initial value from textarea

--- src/RichTextEditor/RichTextEditor.tsx
+++ src/RichTextEditor/RichTextEditor.tsx
@@ -9,7 +9,10 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { nodes } from "./nodes";
-import { ELEMENT_TRANSFORMERS } from "@lexical/markdown";
+import {
+  $convertFromMarkdownString,
+  ELEMENT_TRANSFORMERS,
+} from "@lexical/markdown";
 import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
 import { TreeViewPlugin } from "./plugins";
 
@@ -24,16 +27,23 @@ function onError(error) {
   console.error(error);
 }
 
-const initialConfig: InitialConfigType = {
+const initialConfig = (initialValue: string): InitialConfigType => ({
   namespace: "MyEditor",
+  editorState: () =>
+    $convertFromMarkdownString(initialValue, ELEMENT_TRANSFORMERS),
   theme,
   onError,
   nodes,
+});
+
+type Props = {
+  initialValue: string;
 };
 
-const Editor = () => {
+const Editor: FC<Props> = ({ initialValue }) => {
+  console.log({ initialValue });
   return (
-    <LexicalComposer initialConfig={initialConfig}>
+    <LexicalComposer initialConfig={initialConfig(initialValue)}>
       <MarkdownShortcutPlugin transformers={ELEMENT_TRANSFORMERS} />
       <RichTextPlugin
         contentEditable={<ContentEditable />}
@@ -46,13 +56,13 @@ const Editor = () => {
   );
 };
 
-export const RichTextEditor: FC = () => {
+export const RichTextEditor: FC<Props> = (props) => {
   return (
     <div
       className="write-content tooltipped tooltipped-ne tooltipped-no-delay tooltipped-align-left-1 hide-reaction-suggestion upload-enabled mx-0 mt-2 mb-2 mx-md-2 hx_sm-hide-drag-drop js-reaction-suggestion"
       style={{ width: "100%" }}
     >
-      <Editor />
+      <Editor {...props} />
     </div>
   );
 };
--- src/RichTextTabPanel.tsx
+++ src/RichTextTabPanel.tsx
@@ -1,14 +1,35 @@
-import { FC } from "react";
+import { FC, useCallback, useMemo, useSyncExternalStore } from "react";
 import { RichTextEditor } from "./RichTextEditor";
+
+const useSyncGitHubContentState = () => {
+  const textarea = useMemo(() => {
+    return document.querySelector(
+      "textarea.js-comment-field"
+    ) satisfies HTMLTextAreaElement | null;
+  }, []);
+  const subscribe = useCallback(
+    (onStoreChange: () => void) => {
+      textarea?.addEventListener("input", onStoreChange);
+      return () => textarea?.removeEventListener("input", onStoreChange);
+    },
+    [textarea]
+  );
+  const getSnapshot = useCallback(() => textarea?.value, []);
+  const content = useSyncExternalStore(subscribe, getSnapshot) ?? "";
+
+  return { content };
+};
 
 type Props = {
   enabled: boolean;
 };
 
 export const RichTextTabPanel: FC<Props> = ({ enabled }) => {
+  const { content } = useSyncGitHubContentState();
+
   return (
     <div role="tabpanel" hidden={!enabled}>
-      <RichTextEditor initialValue="foo" />
+      <RichTextEditor initialValue={content} />
     </div>
   );
 };
