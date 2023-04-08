fd62712b0f7a319e4d9ee5fb7233a62b0224bc2d
MH4GF <h.miyagi.cnw@gmail.com>
2023-04-08 15:30:47 +0900

    refactor: move RichTextEditor to its own folder

--- src/RichTextEditor/RichTextEditor.tsx
+++ src/RichTextEditor/RichTextEditor.tsx
@@ -3,13 +3,17 @@ import { FC } from "react";
 import { $getRoot, $getSelection } from "lexical";
 import { useEffect } from "react";
 
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import {
+  InitialConfigType,
+  LexicalComposer,
+} from "@lexical/react/LexicalComposer";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 
 const theme = {
   // Theme styling goes here
@@ -49,16 +53,17 @@ function onError(error) {
   console.error(error);
 }
 
-const Editor = () => {
-  const initialConfig = {
-    namespace: "MyEditor",
-    theme,
-    onError,
-  };
+const initialConfig: InitialConfigType = {
+  namespace: "MyEditor",
+  theme,
+  onError,
+  nodes: [HeadingNode, QuoteNode],
+};
 
+const Editor = () => {
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
+      <RichTextPlugin
         contentEditable={<ContentEditable />}
         placeholder={<div>Enter some text...</div>}
         ErrorBoundary={LexicalErrorBoundary}
